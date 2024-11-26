9dc882ddb479d10287d6edd33060880884efb18c
andrewmanin <andrejmanin54@gmail.com>
2024-11-26 19:03:15 +0200

    Task 5

--- main.cpp
+++ main.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
 using namespace std;
 
-int linSearch(int array[], int size, int key) {
-    for (int i = 0; i < size; i++) {
-        if (array[i] == key) {
-            return i;
+int binSearch(int array[], int size, int key) {
+    int left = 0;
+    int right = size - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if (array[mid] == key) {
+            return mid;
+        }
+
+        if (array[mid] < key) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
         }
     }
+
     return -1;
 }
 
 int main() {
-    int arr[] = {10, 25, 30, 45, 50, 60};
+    int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90};
     int size = std::size(arr);
     int key;
 
     cout << "Введіть елемент для пошуку: ";
     cin >> key;
 
-    int index = linSearch(arr, size, key);
+    int index = binSearch(arr, size, key);
 
     index != -1 ? cout << "Елемент знайдено на індексі: " << index << endl : cout << "Елемент не знайдено в масиві." << endl;
 
