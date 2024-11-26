2a32da2b6055dbef5fe8e0ad54c945dca959cb8a
andrewmanin <andrejmanin54@gmail.com>
2024-11-26 18:58:46 +0200

    Task 4

--- main.cpp
+++ main.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int nsd(int a, int b) {
-    if (b == 0)
-        return a;
-    return nsd(b, a % b);
+int linSearch(int array[], int size, int key) {
+    for (int i = 0; i < size; i++) {
+        if (array[i] == key) {
+            return i;
+        }
+    }
+    return -1;
 }
 
 int main() {
-    int num1, num2;
-    cout << "Введіть два числа: ";
-    cin >> num1 >> num2;
-    cout << "Найбільший спільний дільник: " << nsd(num1, num2) << endl;
+    int arr[] = {10, 25, 30, 45, 50, 60};
+    int size = std::size(arr);
+    int key;
+
+    cout << "Введіть елемент для пошуку: ";
+    cin >> key;
+
+    int index = linSearch(arr, size, key);
+
+    index != -1 ? cout << "Елемент знайдено на індексі: " << index << endl : cout << "Елемент не знайдено в масиві." << endl;
+
     return 0;
 }
